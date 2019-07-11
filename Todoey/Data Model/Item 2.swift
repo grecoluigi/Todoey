233540547c845b8750a0cb514832f0791a21ae12
Luigi Greco <9luigi1@gmail.com>
2019-07-11 17:44:14 +0200

    Persisting data using Codable protocol and Plist.

---
+++ Todoey/Data Model/Item 2.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Luigi Greco on 05/05/2019.
+//  Copyright © 2019 Luigi Greco. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title : String = ""
+    var done : Bool = false
+}
